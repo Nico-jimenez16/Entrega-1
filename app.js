e10bdfbe89d03f3acc9d7d32c90cb00ed502d261
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-04-26 17:48:41 -0300

    commit contador

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = new Vue ({
+    el: '#app',
+    data() {
+        return {
+            titulo: 'Contador',
+            suma: 0,
+            presentacion: 'Angel Nicolas Jimenez',
+            entrega: 'Entrega Nro 1 - Ejercicio Contador'
+        }
+    },
+});
